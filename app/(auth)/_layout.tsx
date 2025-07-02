54819305f41f1d67197b60fa52905f85b9a2657e
Juan Pardo <juan23091988@gmail.com>
2025-07-02 11:54:42 -0300

    feat: update AuthLayout to remove screenOptions and set presentation to default

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,12 +2,7 @@ import { Stack } from 'expo-router';
 
 export default function AuthLayout() {
   return (
-    <Stack
-      screenOptions={{
-        // headerShown: false,
-        presentation: 'card',
-      }}
-    >
+    <Stack>
       <Stack.Screen name='sign-in' options={{ headerShown: false }} />
       <Stack.Screen name='register' />
       <Stack.Screen name='forgot-password' />
